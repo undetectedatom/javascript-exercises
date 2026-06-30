abcc6f342b81b774a517589a5678efda05429c0b
undetectedatom <xcc_kid666@163.com>
2026-06-30 09:05:49 +0000

    learn how to use Number.isInteger to decide a variable's type

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(!Number.isInteger(a) || !Number.isInteger(b) ) {
+        return "ERROR";
+    }
+    if(a <= 0 || b <= 0) {
+        return "ERROR";
+    }
+    if(a > b) {
+        n = a - b + 1;
+    } else {
+        n = b - a + 1;
+    }
+    return (a + b) * n / 2;
 };
 
 // Do not edit below this line
