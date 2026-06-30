e789f94458974a2e6a3c5d552d54ae4c6936cb8b
undetectedatom <xcc_kid666@163.com>
2026-06-30 08:53:47 +0000

    js is very verbose when removing items from arrays

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...restArgs) {
+    let rest = new Set(restArgs);
+    let i = 0;
+    for(let i = array.length - 1; i >= 0; i--) {
+        let item = array[i];
+        if(rest.has(item)) {
+            array.splice(i, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
