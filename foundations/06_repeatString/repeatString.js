f03cdb275e94f52ca45b10b86a14b4ddb7b52d3c
undetectedatom <xcc_kid666@163.com>
2026-06-30 08:32:09 +0000

    finish repeatString about loop

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(content, times) {
+    res = "";
+    if (times < 0){
+        return "ERROR";
+    }
+    for(i = 0; i < times; i ++){
+        res += content;
+    }
+    return res;
 };
 
 // Do not edit below this line
