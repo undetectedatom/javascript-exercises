d228c88ffc3308f53ca2f3308146333bfb97d9d5
undetectedatom <xcc_kid666@163.com>
2026-08-26 03:34:13 +0000

    finish leap year

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,5 @@
 const leapYears = function(year) {
-    if(year % 4 === 0 && (! year % 100 === 0 || year % 400 ===0)) {
+    if(year % 4 === 0 && (! year % 100 === 0 || year % 400 === 0)) {
         return true;
     }
     return false;
