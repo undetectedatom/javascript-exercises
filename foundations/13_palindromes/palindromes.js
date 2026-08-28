421eb1a3cb71eb90e7d4a0c9ea97bb856e656a06
maxwell-laptop <j.maxwell.song@gmail.com>
2026-08-27 21:09:49 -0500

    finish 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.replace(/[,\s.!]+/gi, "");
+  let i = 0,
+    j = str.length - 1;
+  while (i < j) {
+    if (str.at(i).toLowerCase() != str.at(j).toLowerCase()) {
+      return false;
+    }
+    i++;
+    j--;
+  }
+  return true;
 };
 
 // Do not edit below this line
