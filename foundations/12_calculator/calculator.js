9851a78e9e50683e559f377b09b99815cbc30376
maxwell-laptop <j.maxwell.song@gmail.com>
2026-08-27 20:57:45 -0500

    finish 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((last, cur) => last + cur, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((last, cur) => last * cur, 1);
 };
 
-const power = function() {
-	
+const power = function (a, p) {
+  return a ** p;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a == 0) {
+    return 1;
+  }
+  let res = 1;
+  for (let i = 1; i <= a; i++) {
+    res *= i;
+  }
+  return res;
 };
 
 // Do not edit below this line
@@ -29,5 +36,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
